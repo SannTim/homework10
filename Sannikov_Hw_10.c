6454ef14c91fca1e66e3cc1642391af940a66ed9
SannTim <timohaj1@outlook.com>
2022-11-22 18:49:04 +0300

    https://github.com/SannTim/homework10.git

---
+++ Sannikov_Hw_10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <signal.h>
