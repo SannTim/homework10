9e83b1da292275b0a7b2a8477c782301d0200480
SannTim <timohaj1@outlook.com>
2022-11-22 20:38:44 +0300

    Master

--- Sannikov_Hw_10.c
+++ Sannikov_Hw_10.c
@@ -8,3 +8,52 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 #include <signal.h>
+
+int N;
+pid_t son;
+void handleral(int k){
+    N = 0;
+}
+
+void handler1(int k){
+    printf("Father\n");
+    if (N) kill(son,SIGUSR1);
+    signal(SIGUSR1,handler1);
+}
+
+void handler2(int k){
+    printf("Son\n");
+    if (N) kill(getppid(),SIGUSR1);
+    signal(SIGUSR1,handler2);
+}
+void empty(int k){}
+void hanalpr(int k){
+    printf("Father\n");
+    signal(SIGALRM,hanalpr);
+}
+int main(){
+    signal(SIGALRM, handleral);
+    signal(SIGUSR1,handler2);
+    N = 1;
+    
+    son = fork();
+    if (son == 0){
+        N = 1;
+        alarm(2);
+        pause();
+        while (N) pause();
+
+        exit(1);
+    }
+    alarm(2);
+    printf("Father\n");
+    signal(SIGUSR1, handler1);
+    kill(son,SIGUSR1);  
+    pause();  
+    while (N)pause();
+    signal(SIGALRM,hanalpr);
+    alarm(1);
+    while (pause()){
+        alarm(1);
+    }
+}
